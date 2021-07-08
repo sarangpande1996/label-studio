e82e27cc6524177c079e4b44b1eeb5b3d0e158d0
sarangpande1996 <41369635+sarangpande1996@users.noreply.github.com>
2021-07-09 00:08:55 +0530

    Update start_label_studio.sh

--- deploy/start_label_studio.sh
+++ deploy/start_label_studio.sh
@@ -2,4 +2,4 @@
 # see deploy/uwsgi.ini for details
 # /usr/local/bin/uwsgi --ini /label-studio/deploy/uwsgi.ini
 echo "Make simple Label Studio launch..."
-label-studio
+label-studio start my_project --init
